c475a5f8785434a3156ad0aca8d8231751c7d1e0
priyapalsingh <priyapalsingh16@gmail.com>
2025-12-17 08:30:43 +0530

    Time: 1 ms (99.14%), Space: 47.3 MB (36.33%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int jump(int[] nums) {
+        if(nums.length<=1) return 0;
+        int jump=0;
+        int currentJumpedAt=0;
+        int farthest=0;
+        for(int i=0;i<nums.length-1;i++){
+            farthest=Math.max(farthest,i+nums[i]);
+            if(i==currentJumpedAt){
+                jump++;
+                currentJumpedAt=farthest;
+            }
+        }
+        return jump;
+        
+        
+        
+    }
+}
