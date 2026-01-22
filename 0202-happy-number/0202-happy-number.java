9f19e115e528f374ff9c311e8beef300e1d174cf
priyapalsingh <priyapalsingh16@gmail.com>
2026-01-22 19:30:58 +0530

    Time: 1 ms (77.28%), Space: 42.5 MB (39.79%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> seen=new HashSet<>();
+        while(n>1){
+            if(seen.contains(n)) return false;
+            seen.add(n);
+            n=sumofsquare(n);
+        }
+        return true;        
+    }
+    private int sumofsquare(int n){
+        int sum=0;
+        while(n>0){
+            int rem=n%10;
+            sum+=rem*rem;
+            n/=10;
+        }
+        return sum;        
+    }
+}
