f7081d9ee4d19d4ef9c91df42cb599d77e4448b7
priyapalsingh <priyapalsingh16@gmail.com>
2026-02-04 13:03:46 +0530

    Time: 1 ms (15.07%), Space: 42 MB (89.82%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp=new int[m][n];
+        return func(m,n,dp);
+        
+    }
+    private int func(int m, int n, int[][] dp){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+                int up=0,left=0;
+                if(i>0){
+                    up=dp[i-1][j];
+                }
+                if(j>0){
+                    left=dp[i][j-1];
+                }
+                dp[i][j]=up+left;
+            }
+               
+        }
+        return dp[m-1][n-1];
+
+     }
+}
