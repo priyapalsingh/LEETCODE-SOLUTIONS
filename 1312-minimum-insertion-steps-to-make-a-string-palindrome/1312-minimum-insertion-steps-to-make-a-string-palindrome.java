dd7854d7672247baa881780495181458ec90ae8a
priyapalsingh <priyapalsingh16@gmail.com>
2026-02-13 23:07:33 +0530

    Time: 25 ms (65.92%), Space: 47.5 MB (57%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int minInsertions(String s) {
+        int n=s.length();
+        int k=longestpalindromesubstring(s);
+        return n-k;
+    }
+    private int lcs(String s,String t){
+        int m=s.length();
+        int n=t.length();
+        int[][] dp=new int[m+1][n+1];
+
+        for(int i=0;i<=m;i++){
+            dp[i][0]=0;
+        }
+        for(int i=0;i<=n;i++){
+            dp[0][i]=0;
+        }
+        //fill the table
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+
+                }
+                
+            }
+        }
+        return dp[m][n];
+    }
+    private int longestpalindromesubstring(String s){
+        String t=new StringBuilder(s).reverse().toString();
+        return lcs(s,t);
+    }
+
+}
