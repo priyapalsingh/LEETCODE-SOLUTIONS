d7a152531c0fba532bd3fd1a412b21463f741ed7
priyapalsingh <priyapalsingh16@gmail.com>
2026-02-20 11:12:47 +0530

    Time: 0 ms (100%), Space: 42.9 MB (76.06%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open=0;
+        int close=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='('){
+                open++;
+
+            } 
+            else{
+            if(open>0){
+                open--;
+            }else{
+                close++;
+            }
+        }
+        }
+        return close+open;
+        
+    }
+}
