f7d5914857600b4b619d0def380fd0ba0adf86ad
priyapalsingh <priyapalsingh16@gmail.com>
2026-03-28 22:45:44 +0530

    Time: 0 ms (100%), Space: 44 MB (26.09%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // 1. Transpose the matrix
+        // We swap matrix[i][j] with matrix[j][i]
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // 2. Reverse each row
+        // This converts the transposed matrix into a 90-degree rotation
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+}
