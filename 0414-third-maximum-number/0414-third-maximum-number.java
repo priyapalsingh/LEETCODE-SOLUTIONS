73562adb9e2ee18b59e8a7f348d2f1269cdd2367
priyapalsingh <priyapalsingh16@gmail.com>
2026-06-30 18:25:57 +0530

    Time: 3 ms (74.09%), Space: 44.8 MB (60.03%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,15 +1,36 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        List<Integer> list=new ArrayList<>();
-        for(int x:nums){
-            if(!list.contains(x)){
-                list.add(x);
+        // Distinct top 3 maximums ko track karne ke liye objects
+        Integer firstMax = null;
+        Integer secondMax = null;
+        Integer thirdMax = null;
+        
+        for (int num : nums) {
+            // Agar number pehle se hi hamare top 3 me hai, toh duplicates skip karo
+            if ((firstMax != null && firstMax == num) || 
+                (secondMax != null && secondMax == num) || 
+                (thirdMax != null && thirdMax == num)) {
+                continue;
+            }
+            
+            // 1. Agar current number firstMax se bada hai
+            if (firstMax == null || num > firstMax) {
+                thirdMax = secondMax;
+                secondMax = firstMax;
+                firstMax = num;
+            } 
+            // 2. Agar current number firstMax se chota par secondMax se bada hai
+            else if (secondMax == null || num > secondMax) {
+                thirdMax = secondMax;
+                secondMax = num;
+            } 
+            // 3. Agar current number secondMax se chota par thirdMax se bada hai
+            else if (thirdMax == null || num > thirdMax) {
+                thirdMax = num;
             }
         }
-        Collections.sort(list);
-        if(list.size()<=2){
-            return list.get(list.size()-1);
-        }
-        return list.get(list.size()-3);
+        
+        // Agar third maximum exist nahi karta, toh firstMax (maximum) return karo
+        return (thirdMax == null) ? firstMax : thirdMax;
     }
 }
