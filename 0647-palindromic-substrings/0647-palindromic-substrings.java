4ee2ba0274e630cedb4ca99b3e68a1404da21eaa
priyapalsingh <priyapalsingh16@gmail.com>
2026-08-01 23:18:47 +0530

    Time: 29 ms (25.6%), Space: 53.2 MB (5.37%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,26 +1,45 @@
+import java.util.Arrays;
+
 class Solution {
     public int countSubstrings(String s) {
-        int n=s.length();
-        int count=0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<=n;j++){
-                if(isPalindrome(s.substring(i,j))){
+        int n = s.length();
+        
+        // dp[i][j] : -1 (unvisited), 0 (false), 1 (true)
+        int[][] dp = new int[n][n];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        int count = 0;
+
+        // Har possible substring range [i, j] check karo
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (solve(i, j, s, dp) == 1) {
                     count++;
                 }
             }
         }
+
         return count;
     }
-    private boolean isPalindrome(String t){
-        int left=0;
-        int right=t.length()-1;
-        while(left<=right){
-            if(t.charAt(left)!=t.charAt(right)){
-                return false;
-            }
-            left++;
-            right--;
+
+    private int solve(int i, int j, String s, int[][] dp) {
+        // Base Case 1: Single character is always a palindrome
+        if (i >= j) {
+            return 1;
         }
-        return true;
+
+        // DP Memoization Check
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        // Outer characters same hone par inner substring (i+1, j-1) check karo
+        if (s.charAt(i) == s.charAt(j)) {
+            return dp[i][j] = solve(i + 1, j - 1, s, dp);
+        }
+
+        return dp[i][j] = 0;
     }
 }
