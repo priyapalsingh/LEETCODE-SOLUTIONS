3d7a0cc490efc96191732bf4fd60b05a1d07449d
priyapalsingh <priyapalsingh16@gmail.com>
2026-08-02 08:35:06 +0530

    Time: 147 ms (5.01%), Space: 47.9 MB (8.76%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,19 +1,30 @@
 class Solution {
     public int jump(int[] nums) {
-        if(nums.length<=1) return 0;
-        int jump=0;
-        int currentJumpedAt=0;
-        int farthest=0;
-        for(int i=0;i<nums.length-1;i++){
-            farthest=Math.max(farthest,i+nums[i]);
-            if(i==currentJumpedAt){
-                jump++;
-                currentJumpedAt=farthest;
-            }
+        int numlen=nums.length;
+        int[] dp=new int[numlen];
+        Arrays.fill(dp,-1);
+
+        return solve(0,nums,dp);
+    }
+    private int solve(int i,int[] nums,int[] dp){
+        int numlen=nums.length;
+        if(i>=numlen-1){
+            return 0;
         }
-        return jump;
-        
-        
-        
+
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+
+        int minjump=100000;
+        int dest=nums[i];
+        for(int j=1;j<=dest;j++){
+            if(i+j<=numlen-1){
+                int ans=1+solve(i+j,nums,dp);
+                minjump=Math.min(minjump,ans);
+            }
+            
+        }
+        return dp[i]=minjump;
     }
 }
