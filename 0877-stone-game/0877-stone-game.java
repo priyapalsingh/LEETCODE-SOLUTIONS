a0963ac25ffcaa6ea63f06ce2f9a7f87cb2b070e
priyapalsingh <priyapalsingh16@gmail.com>
2026-08-02 08:08:14 +0530

    Time: 9 ms (13.82%), Space: 46.8 MB (16.42%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,23 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n=piles.length;
+        int[][] dp=new int[n][n];
+        for(int[] rows:dp){
+            Arrays.fill(rows,-1);
+        }
+
+        return solve(0,n-1,piles,dp)>0;
+
+    }
+    private int solve(int i,int j,int[] piles,int[][] dp){
+        if(i==j) return i;
+
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int pickleft=piles[i]+solve(i+1,j,piles,dp);
+        int pickright=piles[j]+solve(i,j-1,piles,dp);
+
+        return dp[i][j]=Math.max(pickleft,pickright);
     }
 }
