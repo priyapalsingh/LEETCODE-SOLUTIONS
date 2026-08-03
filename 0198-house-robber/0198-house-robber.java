5dcdb341d8124f7a46ef18dd9619bb549145f94e
priyapalsingh <priyapalsingh16@gmail.com>
2026-08-03 22:13:03 +0530

    Time: 0 ms (100%), Space: 42.8 MB (38.08%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,14 +1,33 @@
+import java.util.Arrays;
+
 class Solution {
     public int rob(int[] nums) {
-        int n=nums.length;
-        int[] dp=new int[n+1];
-        if(n==1) return nums[0];
-        dp[0]=nums[0];
-        dp[1]=Math.max(nums[0],nums[1]);
-        for(int i=2;i<n;i++){
-            //pick or not pick
-            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+        int n = nums.length;
+        int[] dp = new int[n];
+        // Memoization array ko -1 se initialize karte hain
+        Arrays.fill(dp, -1);
+
+        return solve(0, nums, dp);
+    }
+
+    private int solve(int i, int[] nums, int[] dp) {
+        // Base Case: Agar array boundary se bahar nikal gaye
+        if (i >= nums.length) {
+            return 0;
         }
-        return dp[n-1];
+
+        // Return memoized result if already calculated
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+
+        // Choice 1: Rob current house and move to i + 2
+        int rob = nums[i] + solve(i + 2, nums, dp);
+
+        // Choice 2: Skip current house and move to i + 1
+        int skip = solve(i + 1, nums, dp);
+
+        // Store and return max of both choices
+        return dp[i] = Math.max(rob, skip);
     }
 }
