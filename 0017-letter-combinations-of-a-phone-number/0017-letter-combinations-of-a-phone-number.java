bd1e526728eb062571d6d0a11a2aae24b45efb69
priyapalsingh <priyapalsingh16@gmail.com>
2026-08-09 11:39:17 +0530

    Time: 0 ms (100%), Space: 43.7 MB (37.63%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,43 +1,40 @@
 class Solution {
-
-    String[] map = {
-        "",     // 0
-        "",     // 1
-        "abc",  // 2
-        "def",  // 3
-        "ghi",  // 4
-        "jkl",  // 5
-        "mno",  // 6
-        "pqrs", // 7
-        "tuv",  // 8
-        "wxyz"  // 9
-    };
+    String[] map={
+        "" ,//0
+        "" ,//1
+        "abc", //2
+        "def", //3
+        "ghi", //4
+        "jkl", //5
+        "mno", //6
+        "pqrs", //7
+        "tuv" ,//8
+        "wxyz" //9
+    } ; 
 
     public List<String> letterCombinations(String digits) {
-        List<String> result = new ArrayList<>();
-        if (digits.length() == 0) return result;
-
-        backtrack(0, digits, new StringBuilder(), result);
+        List<String> result=new ArrayList<>();
+       if(digits.length()==0){
+        return result;
+       }
+       
+        backtrack(0,digits,result,new StringBuilder());
         return result;
     }
-
-    private void backtrack(int index, String digits,
-                           StringBuilder current,
-                           List<String> result) {
-
-        // base case
-        if (index == digits.length()) {
+    private void backtrack(int index,String digits,List<String> result, StringBuilder current){
+        if(index==digits.length()){
             result.add(current.toString());
-            return;
+            return ;
         }
-
+        StringBuilder sb=new StringBuilder();
         int digit = digits.charAt(index) - '0';
-        String letters = map[digit];
+        String dig=map[digit];
 
-        for (char ch : letters.toCharArray()) {
-            current.append(ch);           // choose
-            backtrack(index + 1, digits, current, result);
-            current.deleteCharAt(current.length() - 1); // undo
+        for(char c:dig.toCharArray()){
+            current.append(c);
+            backtrack(index+1,digits,result,current);
+            current.deleteCharAt(current.length()-1);
         }
+        
     }
 }
