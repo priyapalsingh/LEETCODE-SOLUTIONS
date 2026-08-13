0e4de4289f6fcf319c0abd6d98737d7479a07821
priyapalsingh <priyapalsingh16@gmail.com>
2026-08-13 23:58:56 +0530

    Time: 0 ms (100%), Space: 43 MB (33.83%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,16 +1,5 @@
 class Solution {
     public String toLowerCase(String s) {
-        StringBuilder sb = new StringBuilder();
-
-        for (char c : s.toCharArray()) {
-            // Agar character uppercase hai ('A' se 'Z' ke beech)
-            if (c >= 'A' && c <= 'Z') {
-                sb.append((char) (c + 32)); // Lowercase me convert karke append karo
-            } else {
-                sb.append(c); // Baaki characters (digits/symbols/already lowercase) ko as it is append karo
-            }
-        }
-
-        return sb.toString();
+        return s.toLowerCase();
     }
 }
