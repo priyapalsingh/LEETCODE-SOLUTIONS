c393ca3bbf889524c9b468f15c609f36ec11c56e
priyapalsingh <priyapalsingh16@gmail.com>
2026-09-02 08:41:25 +0530

    Time: 0 ms (100%), Space: 45 MB (73.43%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        //agar array mei sab even hai to already true hoga answer
+        //but agar array mei ek bhi element odd hai toh hum us ek element ko sare even mei se us ek odd ko minus krdo toh saare odd bn jaate hai 
+        //isiliye har case mei true hee return hoga
+
+        return true;
+    }
+}
