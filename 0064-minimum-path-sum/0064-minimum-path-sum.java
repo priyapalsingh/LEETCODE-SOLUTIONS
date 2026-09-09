35b856463b0b58f71a2d39e385368f9cf9b84f66
priyapalsingh <priyapalsingh16@gmail.com>
2026-09-09 09:59:47 +0530

    Time: 1 ms (99.82%), Space: 50 MB (84.91%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -8,33 +8,27 @@ class Solution {
 
         // Create DP table
         int[][] dp = new int[n][m];
-
-        // Fill the table
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-
-                // First cell initialization
-                if (i == 0 && j == 0)
-                    dp[i][j] = matrix[i][j];
-                else {
-                    // Calculate from top
-                    int up = matrix[i][j];
-                    if (i > 0) up += dp[i - 1][j];
-                    else up += (int)1e9;
-
-                    // Calculate from left
-                    int left = matrix[i][j];
-                    if (j > 0) left += dp[i][j - 1];
-                    else left += (int)1e9;
-
-                    // Take minimum
-                    dp[i][j] = Math.min(up, left);
-                }
-            }
+        for(int[] rows:dp){
+            Arrays.fill(rows,-1);
         }
 
-        // Return result
-        return dp[n - 1][m - 1];
+        return helper(matrix,dp,n-1,m-1);
+    }
+    public int helper(int[][] matrix,int[][] dp,int i,int j){
+        if(i==0 && j==0){
+            return matrix[0][0];
+        }
+        if(i<0 || j<0){
+            return Integer.MAX_VALUE/2;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int up=matrix[i][j]+helper(matrix,dp,i-1,j);
+        int left=matrix[i][j]+helper(matrix,dp,i,j-1);
+
+        return dp[i][j]=Math.min(up,left);
+
     }
 }
 
